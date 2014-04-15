7d3adddde4047fb52f1ed1f981c952cba450a304
Tyler Buchanan <tmailbuchanan@yahoo.com>
2014-04-14 23:19:40 -0700

    edit

--- lab/lib/mathexpert.rb
+++ lab/lib/mathexpert.rb
@@ -8,14 +8,21 @@ module MathExpert
   #iterative
   def self.calculate_factorial_iterative n
     #TODO: Implement factorial using a loop
-    n
+    #n = (n) * (n-1) * (n-2) * (n-3)
+      factor = 1
+      for i in 1..n
+        factor = factor * i
+      end
+      factor
   end
 
   #recursive
   def self.calculate_factorial_recursive n
     #TODO: Implement factorial using recursion
-    n
+    if n == 0
+       return 1
+    else
+        n * calculate_factorial_recursive(n-1)
+    end
   end
-
-
 end
--- lab/lib/sorter.rb
+++ lab/lib/sorter.rb
@@ -6,11 +6,21 @@ module Sorter
 
   def self.sort_array numbers
     #TODO: Sort the array of numbers from low to high in the same array and return the array
-    numbers
+   # numbers = [4,6,3,2]
+    
+#can't figure this out :(
+	
+	numbers.sort
+	
+	x = numbers
+	return x
+ 
   end
-
   def self.swap_items_in_array items, indexA, indexB
     #TODO: Swap the items at indexA and indexB and return the items
+ 	 array = items
+    array[indexA], array[indexB] = array[indexB], array[indexA]
+    return array
   end
-
 end
+
