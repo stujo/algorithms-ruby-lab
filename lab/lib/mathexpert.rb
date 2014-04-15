c7d0cf76491692b57a25da43ff057abb8f4fb902
Stuart_Grossman <grossman.stuart1@gmail.com>
2014-04-15 08:19:25 -0700

    done

--- lab/lib/mathexpert.rb
+++ lab/lib/mathexpert.rb
@@ -6,16 +6,29 @@ module MathExpert
   #answer = (n) * (n-1) * (n-2) * (n-3) * (n-4) * (n-5) ...
 
   #iterative
-  def self.calculate_factorial_iterative n
-    #TODO: Implement factorial using a loop
+  def self.calculate_factorial_iterative(n)
+    i = 1
+    result = 1
+    while i <= n 
+    result = result * i
+    i += 1
+    end
+    return result
     n
   end
 
   #recursive
-  def self.calculate_factorial_recursive n
-    #TODO: Implement factorial using recursion
+  def self.calculate_factorial_recursive(n)
+    if n == 0
+      1
+    else
+     n = n * calculate_factorial_recursive(n - 1)
     n
+    end
   end
-
-
 end
+
+
+
+
+
--- lab/lib/randomarraygenerator.rb
+++ lab/lib/randomarraygenerator.rb
@@ -1,7 +1,14 @@
+#TODO: Generate an array of (size) random integers (Between min and max inclusive)
 module RandomArrayGenerator
-  def self.generate_random_array(size, min, max)
-
-    #TODO: Generate an array of (size) random integers (Between min and max inclusive)
-
+def self.generate_random_array(size, min, max)
+  arr = []
+  if size < 0 || min > max  
+  return nil
+  else
+  size.times do 
+  arr.push rand(min..max)
   end
+  end
+  return arr
+end
 end
