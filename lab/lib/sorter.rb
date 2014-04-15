a9a8e303154e0254baadc1fb39a1c8abf0bffa8d
Tyler Buchanan <tmailbuchanan@yahoo.com>
2014-04-15 10:02:57 -0700

    made change to sorter

--- lab/lib/sorter.rb
+++ lab/lib/sorter.rb
@@ -7,8 +7,13 @@ module Sorter
   def self.sort_array numbers
     #TODO: Sort the array of numbers from low to high in the same array and return the array
    # numbers = [4,6,3,2]
-        
-
+    
+#can't figure this out :(
+	
+	numbers.sort
+	
+	x = numbers
+	return x
  
   end
   def self.swap_items_in_array items, indexA, indexB
