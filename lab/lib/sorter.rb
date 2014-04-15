4cc0d3d5b0386e541bb1c30606399e8d09f2338c
blairprophett <blaircuny@gmail.com>
2014-04-15 09:04:56 -0700

    algorithms lab

--- lab/lib/sorter.rb
+++ lab/lib/sorter.rb
@@ -6,11 +6,27 @@ module Sorter
 
   def self.sort_array numbers
     #TODO: Sort the array of numbers from low to high in the same array and return the array
-    numbers
+   # cycles = numbers.length.times
+   # until cycles == 0 do  
+    numbers.each_index do |i|
+    	(numbers.length - i - 1).times do |swap|
+    		if numbers[swap] > numbers[swap + 1]
+    			numbers[swap], numbers[swap + 1] = numbers[swap + 1], numbers[swap]
+    		end
+    	end
+    end
+  numbers
   end
 
   def self.swap_items_in_array items, indexA, indexB
     #TODO: Swap the items at indexA and indexB and return the items
-  end
-
+    # temp = items[indexA]
+    # items[indexA] = items[indexB]
+    # items[indexB] = temp
+    # items
+ 		
+ 		#parallel assignment
+ 		items[indexA], items[indexB] = items[indexA], items[indexB]
+ 		items
+ 	end
 end
